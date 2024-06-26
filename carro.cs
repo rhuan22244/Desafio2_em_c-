7fd4a4304253174d98fc0d0112ffe35e2a7e0572
rhuan barrufi <rhuanbarrufi@gmail.com>
2024-06-26 20:47:21 -0300

    alteração

--- carro.cs
+++ carro.cs
@@ -2,11 +2,13 @@ public class Carro : Veiculo
 {
     public override void Acelerar()
     {
+        Console.WriteLine("Carro acelerando");
         velocidade = velocidade + 10;
     }
 
     public override void Frear()
     {
+        Console.WriteLine("Carro freando");
         velocidade = velocidade - 5;
     }
 }
--- moto.cs
+++ moto.cs
@@ -2,11 +2,13 @@ public class Moto : Veiculo
 {
     public override void Acelerar()
     {
+        Console.WriteLine("Moto acelerando");
         velocidade += 15;
     }
 
     public override void Frear()
     {
+        Console.WriteLine("Moto freando");
         velocidade = velocidade - 15;
     }
 }
